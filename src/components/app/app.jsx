4bd7aac7b0cf3955666e206a186618fbc950eda2
Ivan <vsem2004@gmail.com>
2021-02-07 16:02:55 +0300

    Маршрутизация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import MainScreeen from '../main-screen/main-screen';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import AddReview from '../add-review/add-review';
+import Film from '../film/film';
+import MyList from '../my-list/my-list';
+import NotFound from '../not-found/not-found';
+import Player from '../player/player';
+import SignIn from '../sign-in/sign-in';
 
 const App = (props) => {
   const {name, genre, date, numbers} = props;
 
 
   return (
-    <MainScreeen name={name} genre={genre} date={date} numbers={numbers}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreeen name={name} genre={genre} date={date} numbers={numbers}/>
+        </Route>
+        <Route exact path="/login">
+          <SignIn/>
+        </Route>
+        <Route exact path="/mylist">
+          <MyList/>
+        </Route>
+        <Route exact path="/films/:id">
+          <Film/>
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview/>
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+        <Route>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/not-found/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <section>
+      <h1>404. Page not found :(</h1>
+      <Link to="/">Вернуться на главную</Link>
+    </section>
+  );
+};
+
+
+export default NotFound;
