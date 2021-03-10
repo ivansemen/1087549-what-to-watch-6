a0f988cca8aa9731578516dfe70c7e71ab36b942
Ivan <vsem2004@gmail.com>
2021-03-10 22:29:31 +0300

    Создаст action и reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+import {ActionType} from './action';
+import {keysToCamel} from '../utils/utils';
+import movieListMock from '../mocks/films';
+
+const correctFilms = movieListMock.map((film) => keysToCamel(film));
+
+const initialState = {
+  genre: `All genres`,
+  movieList: correctFilms,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_GENRE:
+      return {
+        ...state,
+        genre: action.genre
+      };
+    case ActionType.GET_MOVIE_LIST:
+      return {
+        ...state,
+        movieList: action.movieList
+      };
+  }
+
+  return state;
+};
+
+export {reducer};
