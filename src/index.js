e8a462ca61ef90f52c88dde62eb70a58eaa5dea7
Ivan <vsem2004@gmail.com>
2021-03-10 22:30:28 +0300

    Подключит redux к проекту

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import films from './mocks/films';
 import {keysToCamel} from './utils/utils';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const correctFilms = films.map((film) => keysToCamel(film));
 // Меняет названия свойств с snake_case на camelCase
 
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
-    <App films={correctFilms}/>,
+    <Provider store={store}>
+      <App films={correctFilms}/>
+    </Provider>,
     document.querySelector(`#root`)
 );
